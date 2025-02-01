0cecaa780424191033841ec9659192384d5d1d9d
Sebastian <sersfa27@gmail.com>
2025-02-01 09:21:48 -0500

    Subir Proyecto

---
+++ Curso_Java/src/HolaMundo.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+        
+        
+    }
+}
